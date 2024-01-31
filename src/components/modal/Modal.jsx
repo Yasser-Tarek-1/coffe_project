89ef5ebc4125d5f811cfa5ca7e9fb52119b71fbe
Yasser Tark <86326663+Yasser-Tarek-1@users.noreply.github.com>
2024-01-31 20:10:50 +0200

    Update Modal.jsx

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -10,7 +10,7 @@ const Modal = ({ isOpen, closeModalHandler, children, height }) => {
         }`}
       ></div>
       <div
-        className={`absolute z-50 bg-white w-[80%]  left-[50%] rounded-[11px] overflow-hidden top-[50%] translate-y-[-50%] translate-x-[-50%] transition-all ${
+        className={`fixed max-w-[300px] z-50 bg-white w-[75%]  left-[50%] rounded-[11px] overflow-hidden top-[50%] translate-y-[-50%] translate-x-[-50%] transition-all ${
           isOpen ? "opacity-100 visible" : "opacity-0 invisible"
         }
         ${height && height}
