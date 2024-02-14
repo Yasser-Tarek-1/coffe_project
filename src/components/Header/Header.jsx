59db0035c89734621ef5f6dd62a86dfa2ef3ab98
yasser <siryasser2020@gmail.com>
2024-02-14 16:28:06 +0200

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
       {/* Top Header */}
       <div className="flex items-center justify-between mx-8 pt-[53px]">
         <div className="relative">
-          <button
+          {/* <button
             onClick={() => setIsOpen(!isOpen)}
             className="w-12 h-7 border-[2px] border-primary rounded-lg cursor-pointer flex items-center justify-evenly"
           >
@@ -36,13 +36,13 @@ const Header = () => {
                 className="w-[9px] block h-full"
               />
             </p>
-          </button>
-          {/* <button onClick={() => setIsOpen(!isOpen)} className="bt-lang">
+          </button> */}
+          <button onClick={() => setIsOpen(!isOpen)} className="bt-lang">
             <p className="text">{lan == "ar" ? "AR" : "EN"}</p>
             <p className="img">
               <img src={arrow} alt="arrow_icon" />
             </p>
-          </button> */}
+          </button>
           {isOpen && (
             <div className="absolute bg-primary uppercase text-base font-normal text-white left-0 top-[26px] rounded-lg w-12 flex items-center flex-col">
               <button onClick={() => switchLanguage("ar")}>AR</button>
